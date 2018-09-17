88b31ceb69237fcf614e4588b1925f751d979302
Vivien T <vivien.traineau@gmail.com>
2018-09-17 15:50:29 -0400

    first commit

---
+++ src/main/java/mcgill_ecse429/arti1/App.java
@@ -0,0 +1,13 @@
+package mcgill_ecse429.arti1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
